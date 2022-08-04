5c99cce6ac803d13bbc3d8ec864745c049cea7e9
vaal12 <qwe1@qwe.com>
2022-08-04 23:27:00 +0300

    1. Added pillow library for ImageField in model
    2. Initial project DB model
       2.1 User based on Abstract user
       2.2 User registered with admin site

--- crossfit/crossfit/settings.py
+++ crossfit/crossfit/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'db'
 ]
 
 MIDDLEWARE = [
@@ -110,6 +111,10 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+
+AUTH_USER_MODEL = 'db.User'
 
 
 # Internationalization
