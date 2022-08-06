974317a265dacd2e857472da384cd4593180fb1a
vaal12 <qwe1@qwe.com>
2022-08-06 11:17:38 +0300

    Changes to settings.py to allow model image generation.

--- crossfit/crossfit/settings.py
+++ crossfit/crossfit/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'db'
+    'django_extensions',
+    'db',
 ]
 
 MIDDLEWARE = [
