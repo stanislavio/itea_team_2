416bf1080cd15c8d06fc524d116e7c974ff4bb60
vaal12 <qwe1@qwe.com>
2022-08-12 09:57:03 +0300

    Bootstrap added to static files (CSS and JS)
    Base template and home page template done (initial version)

--- frontend/userPage_urls.py
+++ frontend/userPage_urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import userPage_views
 
 urlpatterns = [
-    path('', userPage_views.profile)
+    path('', userPage_views.profile),
+    path('test_base/', userPage_views.test_base)
 ]
--- frontend/userPage_views.py
+++ frontend/userPage_views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def profile(request):
     return render(request, 'user_page.html')
+
+def test_base(request):
+    return render(request, 'base.html')
