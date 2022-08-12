47b65243985d553e3ce3eaf843e83815c69b9355
vaal12 <qwe1@qwe.com>
2022-08-12 10:22:23 +0300

    Create stub URLs, Views and templates for main navigation panel items.

--- frontend/userPage_views.py
+++ frontend/userPage_views.py
@@ -4,5 +4,15 @@ from django.shortcuts import render
 def profile(request):
     return render(request, 'user_page.html')
 
-def test_base(request):
-    return render(request, 'base.html')
+def register_user(request):
+    return render(request, 'register_user.html')
+
+def login_user(request):
+    return render(request, 'login_user.html')
+
+def friends_posts(request):
+    return render(request, 'friends_posts.html')
+
+def profile_user(request):
+    return render(request, 'profile_user.html')
+    
