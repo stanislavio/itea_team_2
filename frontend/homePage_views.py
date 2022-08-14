1eef2c05372ec9658d14b4c7dcf64eb463c30d5b
vaal12 <qwe1@qwe.com>
2022-08-14 10:40:04 +0300

    User registration form (unstyled) working
    User password validators disabled

--- frontend/homePage_views.py
+++ frontend/homePage_views.py
@@ -1,9 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# from django import settings
-
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
 
 def index(request):
     # return HttpResponse("This is home page, welcome")
     return render(request, 'home_page.html')
+
+def register_user(request):
+    form = CreateUserForm()
+    if request.method == "POST":
+        print(request.POST)
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            print("Form is valid")
+            form.save()
+    context = {'form' : form}
+    return render(request, 'register_user.html', context)
+
+def login_user(request):
+    return render(request, 'login_user.html')
